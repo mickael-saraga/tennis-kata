1ab9f1e89d65f93e00a7121906b7940864de49f8
mickael-saraga <mickael.m.saraga@gmail.com>
2019-04-08 23:25:32 +0200

    First commit

---
+++ src/main/java/com/kata/tennis/StartGame.java
@@ -0,0 +1,13 @@
+package com.kata.tennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartGame {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(StartGame.class);
+  }
+  
+}
